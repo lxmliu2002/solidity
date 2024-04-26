32e34d3f1ea0499d5247db16964c88622534c9bc
lxmliu2002 <lxmliu2002@126.com>
2024-04-27 00:33:10 +0800

    update delete_batch but need to de some bugs

---
+++ solidity/newnew copy/codes/0xc386662e3ea04b58656d99bd27719d91447e66d6_StakingProxyConvex/delete_StakingProxyConvex.sol
@@ -0,0 +1,8 @@
+/**
+ *Submitted for verification at Etherscan.io on 2022-11-04
+*/
+
+// File: contracts\interfaces\ICurveConvex.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
---
+++ solidity/newnew copy/codes/0xe724e8da0e4de61bd8c04cc5dce9622ec0627fd7_ai/delete_ai.sol
@@ -0,0 +1,6 @@
+/**
+ *Submitted for verification at Etherscan.io on 2022-08-02
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.12;
